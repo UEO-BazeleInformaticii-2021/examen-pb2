6c8ac8d368937520ff1ed04e4e224d06b059a754
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-28 09:55:44 +0200

    start looking into probl2 specs

---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Classes:
+            /*
+                Point:
+                      Props: X, Y (float/double/decimal)
+                            - public read
+                            - private write
+                      Ctor with params
+            */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
